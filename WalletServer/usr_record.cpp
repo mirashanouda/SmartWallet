e39f560c5c99cbf5453d25351950f8573b1029f8
mirashanouda <mira.shanouda@aucegypt.edu>
2023-05-13 21:32:49 +0300

    removed QObject class and create a regular std class

---
+++ WalletServer/usr_record.cpp
@@ -0,0 +1,6 @@
+#include "usr_record.h"
+
+usr_record::usr_record()
+{
+
+}
---
+++ WalletServer/usr_record.h
@@ -0,0 +1,11 @@
+#ifndef USR_RECORD_H
+#define USR_RECORD_H
+
+
+class usr_record
+{
+public:
+    usr_record();
+};
+
+#endif // USR_RECORD_H
